a3f453beecd27e9d0a21c61a3b4ff1652097120b
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2020-12-14 17:34:35 -0800

    Added stateful randomword generator widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,28 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           //child: Text('Hello World'),   // Replace this text...
-          child: Text(wordPair.asPascalCase), // With this text.
+          //child: Text(wordPair.asPascalCase), // Replace this text.
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+// #docregion RandomWordsState, RWS-class-only
+class RandomWordsState extends State<RandomWords> {
+  // #enddocregion RWS-class-only
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+  // #docregion RWS-class-only
+}
+// #enddocregion RandomWordsState, RWS-class-only
+
+// #docregion RandomWords
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
